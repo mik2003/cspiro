4311e22029bb665f9b0adb05cb21a85f390d80d6
mik2003 <62058516+mik2003@users.noreply.github.com>
2024-05-18 23:11:53 +0200

    Add spirograph documentation

--- include/spirograph.h
+++ include/spirograph.h
@@ -3,7 +3,47 @@
 
 #include "matrix.h"
 
+/**
+ * Function to calculate a range of angles.
+ * Calls `mat2d_range`.
+ *
+ * Parameters
+ * ----------
+ * ti : float
+ *     Starting angle of the range (included).
+ * tf : float
+ *     Ending angle of the range (included).
+ * n : int
+ *     Steps in the range (output will have `n + 1` points).
+ *
+ * Returns
+ * -------
+ * Mat2D *
+ *     Pointer to the output matrix (will be of shape `1` by `n + 1`).
+ * NULL
+ *     If memory allocation failed.
+ */
 Mat2D *angles(float ti, float tf, int n);
+
+/**
+ * Function to calculate a spirograph.
+ *
+ * Parameters
+ * ----------
+ * l : float
+ *     Ratio between rho an r (see README for definitions).
+ * k : float
+ *     Ratio between r and R (see README for definitions).
+ * t : Mat2D *
+ *     Pointer to the range of angles over which to calculate the spirograph.
+ *
+ * Returns
+ * -------
+ * Mat2D *
+ *     Pointer to the spirograph matrix.
+ * NULL
+ *     If memory allocation failed or wrong t shape.
+ */
 Mat2D *spirograph(float l, float k, Mat2D *t);
 
 #endif /* SPIROGRAPH_H */
